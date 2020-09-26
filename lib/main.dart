b013ce1f0658c6832ab3808d9725bc64cd14d1a3
ritikadhawan <ritikadhawan28@gmail.com>
2020-09-26 12:49:09 +0530

    button

--- lib/main.dart
+++ lib/main.dart
@@ -19,13 +19,26 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.teal[500],
       ),
       body: Center(
-        //use child property to nest widgets
+          //use child property to nest widgets
 
-        //use NetworkImage for unsaved image from internet
-        //AssetImage widget for saved images with local url
+          //use NetworkImage for unsaved image from internet
+          //AssetImage widget for saved images with local url
 
-        //you can use image.asset instead of assetimage or image.network instead of network image nested inside image
-        child: Image.asset('assets/space-img.jpg'),
+          //you can use image.asset instead of assetimage or image.network instead of network image nested inside image
+          // child: Image.asset('assets/space-img.jpg'),
+
+          child: RaisedButton.icon(
+            onPressed: () {
+              print('You clicked me');
+            },
+            icon: Icon(
+              Icons.mail
+            ),
+            label: Text('mail me'),
+            color: Colors.amber,
+          ),
+
+
         ),
 
         floatingActionButton: FloatingActionButton(
